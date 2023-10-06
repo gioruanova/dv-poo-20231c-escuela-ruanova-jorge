061c7c474f18a4de165c61fc7aaf088ed7127568
Jorge Ruanova <jorge.r@saffire.com>
2023-10-06 18:12:59 -0300

    updates2

--- src/domain/Escuela.java
+++ src/domain/Escuela.java
@@ -7,29 +7,29 @@ public class Escuela {
 	private static final int CANTIDAD_ASIGNACIONES_DEFAULT = 4;
 	private String nombreEscuela;
 	private Asignacion[] asignaciones;
-	private int cantidadAsignaciones;
 
 	public Escuela() {
 		this.asignaciones = new Asignacion[CANTIDAD_ASIGNACIONES_DEFAULT];
-		this.cantidadAsignaciones = 0;
-
 	}
 
 	public Escuela(int capacidad, String nombreEscuela) {
 		this();
 		this.asignaciones = new Asignacion[capacidad];
-		this.cantidadAsignaciones = 0;
 		this.nombreEscuela = nombreEscuela;
 	}
 
 	public void agregarAsignacion(Asignacion asignacion) {
+		int i = 0;
+		while (i < asignaciones.length) { 
+			if (this.asignaciones[i] == null) {
+				this.asignaciones[i] = asignacion;
+				break;
+			}
+			i++;
+		}
 
-		if (cantidadAsignaciones < asignaciones.length) {
-			asignaciones[cantidadAsignaciones] = asignacion;
-			cantidadAsignaciones++;
-
-		} else {
-			System.out.println("Nro Asignaciones máxima alcanzada(" + cantidadAsignaciones + ").");
+		if (i >= asignaciones.length) {
+			System.out.println("Maximo de asignaciones alcanzadas");
 		}
 	}
 
